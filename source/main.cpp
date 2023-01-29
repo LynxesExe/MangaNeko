c40576e4b8e8b7ccb04b586e2ffe05cfc68fc906
Pietro De Domenico <pietrodd2002@gmail.com>
2023-01-29 18:44:28 +0100

    Moved max body size and max memory body size to config file

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,5 @@
 #include <drogon/HttpAppFramework.h>
 int main() {
-    drogon::app().loadConfigFile("../config/config.json").setClientMaxBodySize(2147483647).setClientMaxMemoryBodySize(10 * 1024 * 1024).run();
+    drogon::app().loadConfigFile("../config/config.json").run();
     return 0;
 }
