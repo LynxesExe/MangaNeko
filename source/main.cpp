1bae182e800ff4df7c4e1a204473f28abde1844d
Pietro De Domenico <pietrodd2002@gmail.com>
2023-01-31 19:22:08 +0100

    Added sample code giving issues on VS22 Cmake WSL

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,20 @@
 #include <drogon/HttpAppFramework.h>
+#include "utils/config.h"
+#include "utils/bit7zipLibrarySingleton.h"
+#include <bit7z/bitfileextractor.hpp>
+#include <filesystem>
+
 int main() {
-    drogon::app().loadConfigFile("../config/config.json").run();
+    //bit7z::Bit7zLibrary lib{ "../dependencies/7z.so" };
+
+    std::filesystem::path p("/tmp");
+    std::cout << "Path: " << p.c_str() << std::endl;
+
+    const bit7z::Bit7zLibrary& lib = Bit7ZipLibrarySingleton::getBit7zLibrarySingleton();
+    std::cout << "Config path: " << MangaNeko::globalConfiguration.configFilePath << std::endl;
+    std::cout << "Entrypoint : " << MangaNeko::globalConfiguration.entryPointPath << std::endl;
+    bit7z::BitFileExtractor extractor{ lib, bit7z::BitFormat::Zip };
+    extractor.extract("/tmp/manganeko/uploads/Drag-On Dragoon Utahime Five Manga.cbz", "/tmp/manganeko/cache");
+    drogon::app().loadConfigFile(MangaNeko::globalConfiguration.configFilePath).run();
     return 0;
 }
