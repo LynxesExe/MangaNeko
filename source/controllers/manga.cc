910db35f4a477cf1420268946b5ee637c93bd6c2
Pietro De Domenico <pietrodd2002@gmail.com>
2023-02-01 21:11:34 +0100

    Returns basic error if a file with the same name already exists

--- source/controllers/manga.cc
+++ source/controllers/manga.cc
@@ -1,4 +1,6 @@
 #include "manga.h"
+#include "../utils/config.h"
+#include <filesystem>
 
 void Manga::uploadManga(const HttpRequestPtr& req,
     std::function<void(const HttpResponsePtr&)>&& callback) const
@@ -10,14 +12,24 @@ void Manga::uploadManga(const HttpRequestPtr& req,
     {
         resp->setStatusCode(k400BadRequest);
         resp->addHeader("manga-neko-error", "No file found or 0 length request received.");
-        callback(resp);
     }
     for (auto& file : fileUpload.getFiles())
     {
         // TODO: Register file on the DB here!
+        // TODO: Find a better way to return errors to the user
         if (file.fileLength() > 0)
         {
-            file.saveAs(file.getFileName());
+            std::filesystem::path currentFilePath(MangaNeko::globalConfiguration.entryPointPath);
+            currentFilePath.append(file.getFileName());
+            if (std::filesystem::exists(currentFilePath))
+            {
+                resp->setStatusCode(k207MultiStatus);
+                resp->addHeader("manga-neko-error", "A file with the same name already exists.");
+            }
+            else
+            {
+                file.saveAs(file.getFileName());
+            }
         }
         else
         {
@@ -26,6 +38,18 @@ void Manga::uploadManga(const HttpRequestPtr& req,
         }
     }
 
+    resp->setStatusCode(k200OK);
+    callback(resp);
+}
+
+void Manga::getPage(const HttpRequestPtr& req,
+    std::function<void(const HttpResponsePtr&)>&& callback,
+    std::string&& mangaId, unsigned int chapter, unsigned int page) const
+{
+    std::cout << "Requested manga ID: " << mangaId << std::endl;
+    std::cout << "Chapter: " << chapter << std::endl;
+    std::cout << "Page: " << page << std::endl;
+    auto resp = HttpResponse::newHttpResponse();
     resp->setStatusCode(k200OK);
     callback(resp);
 }
